69a92fc7e4d08e24cc9dd7f8566132b351f2453f
Angad Manroy <angadmanroy@gmail.com>
2022-06-26 01:11:39 +0530

    update

---
+++ 2.2D_arrays/4.Image_Rotation_Method_2.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print_array(int a[][1000],int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
+
+void rotate(int a[][1000],int n){
+    //reverse row 
+    for(int row=0;row<n;row++){
+        int sc=0,ec=n-1;
+        while(sc<ec){
+            swap(a[row][sc],a[row][ec]);
+            sc++;
+            ec--;
+        }
+    }
+
+    //transpose matrix
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            swap(a[i][j],a[j][i]);
+        }
+    }
+
+    //print matrix
+    print_array(a,n);
+}
+
+void rotate_stl(int a[][1000],int n){
+    int i=0;
+    //Reverse row using transpose
+    for(;i<n;i++){
+    reverse(a[i],a[i]+n);
+    }
+    //transpose
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            swap(a[i][j],a[j][i]);
+        }
+    }
+
+    //print matrix
+    print_array(a,n);
+}
+
+int main(){
+    int a[1000][1000];
+    int i,j,k=1,n;
+
+    cin>>n;
+
+    for ( i = 0; i < n; i++)
+    {
+        for ( j = 0; j < n; j++)
+        {
+            a[i][j] = k;
+            k++;
+        }
+        
+    }
+    rotate(a,n);
+    rotate_stl(a,n);
+}
---
+++ 2.2D_arrays/5.Staircase_search.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a[4][4] = { 1,  4,  8, 10,
+                    2,  5,  9, 15,
+                    6, 16, 18, 20,
+                    11,17, 18, 23};
+    int ptr[2] = {0,3};
+    int key;
+    cin>>key;
+    
+}
